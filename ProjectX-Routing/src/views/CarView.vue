e2bef971f651c6df02ec9bcf982329775b7a0ac3
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-07-23 14:33:55 +0530

    dynamic route

---
+++ ProjectX-Routing/src/views/CarView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import cars from "../data/cars.json"
+
+const route = useRoute()
+// route.params: dict[str, str]
+
+// id is the name of the paramater given the router/index.js
+const car = cars.find(c => c.id === parseInt(route.params.id))
+
+</script>
+
+<template>
+    <h1>car view</h1>
+    <br>
+    <hr>
+    <p>{{ car.name }}</p>
+    <p>{{ car.price }}</p>
+    <p>{{ car.year }}</p>
+</template>
--- ProjectX-Routing/src/views/HomeView.vue
+++ ProjectX-Routing/src/views/HomeView.vue
@@ -1,3 +1,19 @@
+<script setup>
+import cars from "../data/cars.json"
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
     <h1>home view</h1>
+    <br>
+    <hr>
+    <div>
+        <ul>
+            <li v-for="car in cars" :key="car.id">
+                <RouterLink :to="`/car/${car.id}`">
+                    {{ car.name }}
+                </RouterLink>
+            </li>
+        </ul>
+    </div>
 </template>
