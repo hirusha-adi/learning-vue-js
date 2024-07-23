79c4d1404f6ad74b6c3db622ef4be25baa9d36dd
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-07-23 14:17:10 +0530

    proper navbar

--- ProjectX-Routing/src/components/Navbar.vue
+++ ProjectX-Routing/src/components/Navbar.vue
@@ -1,10 +1,28 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
     <div>
-        <a href="/">
-            <h1>Home</h1>
-        </a>
-        <a href="/about">
-            <h1>About</h1>
-        </a>
+
+        <!-- Doing this will refresh the page, so, its bad with states (ref) -->
+        <!-- <a href="/">Home</a>
+        <a href="/about">About</a> -->
+
+        <!-- use RouterLink to visit outer route without affecting the states -->
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
+
+        <!-- this link doesn't exist, but we will go to it -->
+        <!-- active class does not work on here -->
+        <RouterLink active-class="active" to="/xxx">About</RouterLink>
+
+        <!-- active-class: is the class to use when that page is active -->
     </div>
 </template>
+
+<style scoped>
+.active {
+    color: red;
+}
+</style>
